fe1aa6ec297ff4be82e493415a480631cead14d1
GajenderTyagi <gajender@programmers.io>
2020-01-25 15:22:46 +0530

    some more features

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function(){
@@ -18,6 +19,10 @@ app.on('ready', function(){
         slashes: true
 
     }));
+    // Quit app when closed 
+    mainWindow.on('close', function(){
+        app.quit();
+    })
 
     // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
@@ -26,23 +31,56 @@ app.on('ready', function(){
 
 });
 
-// create menu template 
-const mainMenuTemplate = [ 
+//Handle craete Add window 
+function craeteAddWindow(){
+    // create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height:200,
+        title:'List whatever you want'
+    });
+    // load html file in window
+    addWindow.loadURL(url.format({
+        pathname : path.join(__dirname, 'addWindow.html'),
+        protocol:'file:',
+        slashes: true
+
+    }));
+    // Garbage collection handle 
+    addWindow.on('close', function(){
+        addWindow= null ; 
+    });
+}
+
+// create menu template
+const mainMenuTemplate = [
     {
         label:'File',
         submenu:[
             {
-                label:'Add Item'
+                label:'Add Item',
+                click(){
+                    craeteAddWindow();
+
+                    
+                }
             },
             {
-                label:'CLear Items'
+                label:'Clear Items'
             },
             {
                 label:'Quit',
+                accelerator: process.platform == 'darwin' ? 'command+Q' :
+                 'Ctrl+Q',
                 click(){
                     app.quit();
-                }    
+                }
             }
         ]
     }
 ];
+
+//If mac, add empty object to menu 
+if(process.platform =='darwin'){
+    mainMenuTemplate.unshift({});
+}
